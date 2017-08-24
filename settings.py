aa936ed1a1ca193f1de2c5641cb1d861362bc176
Ryan Luo <1445542900@qq.com>
2017-08-24 15:57:58 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""相关设置"""
+import os
+
+MONGODB_HOST = "some_host_string"
+MONGODB_PORT = 27017
+MONGODB_DBNAME = 'some_db'
+MONGODB_USERNAME = 'username'
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
