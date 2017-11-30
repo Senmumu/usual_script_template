3de0357c7f930824bb38a81d6aa05576c48129dd
Ryan Luo <1445542900@qq.com>
2017-11-30 16:08:17 +0800

    Update settings.py
    update neo4j and mysqk settings

--- settings.py
+++ settings.py
@@ -14,3 +14,17 @@ REDIS_HOST = 'some_host_string'
 REDIS_PORT = 6379
 REDIS_DB = 0
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
+# Mysql Settings 
+MYSQL_HOST = '192.168.0.110'
+MYSQL_PORT = 3306
+MYSQL_DBNAME = "some_db"
+MYSQL_USERNAME = "username"
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+
+#Neo4j Setings
+NEO4J_HOST = '192.168.0.97'
+NEO4J_PORT = 7474
+#NEO4J_DBNAME = "disease"
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
