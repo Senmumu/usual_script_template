903c0d75d31bde7f0a5cce35ec1ef83c8f9e6dc4
Ryan Luo <1445542900@qq.com>
2018-01-04 02:36:16 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,15 +16,22 @@ REDIS_DB = 0
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
 
 # Mysql Settings 
-MYSQL_HOST = '192.168.0.110'
+MYSQL_HOST = 'some_host_string'
 MYSQL_PORT = 3306
 MYSQL_DBNAME = "some_db"
 MYSQL_USERNAME = "username"
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 
 #Neo4j Setings
-NEO4J_HOST = '192.168.0.97'
+NEO4J_HOST = 'some_host_string'
 NEO4J_PORT = 7474
 #NEO4J_DBNAME = "disease"
 NEO4J_USERNAME = "neo4j"
 NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
+
+# Coach Settings
+COACH_HOST = 'some_host_string' 
+COACH_PORT = 26257
+COACH_DB = 'bank'
+COACH_USER = 'username'
+COACH_PASSWORD = os.getenv('COACH_PASSWORD')
